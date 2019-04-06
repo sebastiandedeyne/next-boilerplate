f420513a478c717c205d0fdd36839a7b11ae8d70
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2019-04-06 10:29:24 +0200

    Update boilerplate

--- pages/_app.js
+++ pages/_app.js
@@ -2,25 +2,25 @@ import React from "react";
 import App, { Container } from "next/app";
 
 class MyApp extends App {
-    static async getInitialProps({ Component, ctx }) {
-        let pageProps = {};
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
 
-        if (Component.getInitialProps) {
-            pageProps = await Component.getInitialProps(ctx);
-        }
-
-        return { pageProps };
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
     }
 
-    render() {
-        const { Component, pageProps } = this.props;
+    return { pageProps };
+  }
 
-        return (
-            <Container>
-                <Component {...pageProps} />
-            </Container>
-        );
-    }
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <Container>
+        <Component {...pageProps} />
+      </Container>
+    );
+  }
 }
 
 export default MyApp;
